f5613c2bff311ea9af5a6c298c959904db29670c
obinnachukz <105755752+obinnachukz@users.noreply.github.com>
2022-09-28 13:43:46 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,24 @@
 
 #Build gitlab/gitlab-ce image
 docker build -t gitlab/gitlab-ce:rhombus $(PWD)
+
+# Set up volume location
+export GITLAB_HOME=$HOME/gitlab
+
+# Install Docker engine
+sudo docker run --detach \
+  --hostname gitlab.example.com \
+  --env GITLAB_ROOT_PASSWORD="Rhombus2022"
+  --publish 443:443 --publish 8080:80 --publish 1001:22 \
+  --name gitlab/gitlab-ce \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  --shm-size 2gb \
+  gitlab/gitlab-ce:latest
+
+# echo user name and password to login to gitlab GUI
+echo "GIT-LAB URL: " "localhost:8080" "if not available, change port to 443 or 1001"
+echo "GIT-LAB Username =" "root"
+echo "GIT-LAB password =" $GITLAB_ROOT_PASSWORD
