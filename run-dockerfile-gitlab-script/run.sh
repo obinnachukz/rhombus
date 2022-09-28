f851ec7b0e832d8131b46bf77c12068c3365b474
obinnachukz <105755752+obinnachukz@users.noreply.github.com>
2022-09-28 12:37:42 -0400

    Update run.sh

--- run-dockerfile-gitlab-script/run.sh
+++ run-dockerfile-gitlab-script/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 #Build gitlab/gitlab-ce image
-
 docker build -t gitlab/gitlab-ce:rhombus $(PWD)
+
+# echo user name and password to login to gitlab GUI
+echo "GIT-LAB URL: " "localhost:8080" "if not available, change port to 443 or 1001"
+echo "GIT-LAB Username =" "root"
+echo "GIT-LAB password =" $GITLAB_ROOT_PASSWORD
