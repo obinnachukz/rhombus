8ede8aed7868bce7b4b09b000c561d5a1e5ff7dd
obinnachukz <105755752+obinnachukz@users.noreply.github.com>
2022-09-28 13:14:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# echo user name and password to login to gitlab GUI
-echo "GIT-LAB URL: " "localhost:8080" "if not available, change port to 443 or 1001"
-echo "GIT-LAB Username =" "root"
-echo "GIT-LAB password =" $GITLAB_ROOT_PASSWORD
+
 
 echo **********************************************************************************
 
